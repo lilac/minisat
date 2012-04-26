23bb8291ea356321853d599a861b1da3a6001e51
James Deng <cnJamesDeng@gmail.com>
2012-04-26 11:49:22 +1000

    Update the Dimacs.h to support % in the cnf format.

--- minisat/core/Dimacs.h
+++ minisat/core/Dimacs.h
@@ -65,7 +65,10 @@ static void parse_DIMACS_main(B& in, Solver& S) {
             }
         } else if (*in == 'c' || *in == 'p')
             skipLine(in);
-        else{
+        else if (*in == '%') {
+            skipLine(in);
+            break;
+        } else{
             cnt++;
             readClause(in, S, lits);
             S.addClause_(lits); }
